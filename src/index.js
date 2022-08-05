c12746f65b67e250dfc9461b428906b406d804e8
VoVFuN <vovfun@gmail.com>
2022-08-05 16:32:53 +0200

    React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {FunctionComponent, ClassComponent} from "./example";
+import {App} from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const age = 23;
+const films = [
+  {title: "film 1", year: 2005},
+  {title: "film 2", year: 2009},
+];
+const handleClick = (film) => {
+  console.log("Click", film);
+};
+
+const Parent = () => {
+  return (
+    <div>
+      <h1 style={{color: "red"}}>Parent Component</h1>
+      <FunctionComponent age={age}
+                         films={films}
+                         handleClick={handleClick}
+      />
+      <hr/>
+      <ClassComponent age={age}
+                      films={films}
+                      handleClick={handleClick}
+      />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
